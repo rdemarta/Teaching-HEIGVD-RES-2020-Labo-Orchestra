eb630c4b86c2919ce07b9329e7ec11b4f4a14f3e
Robin <r.demarta@outlook.com>
2020-06-05 11:52:55 +0200

    Added auditor test

---
+++ docker/image-auditor/src/auditor.js
@@ -0,0 +1,13 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+const multicastAddress = '239.255.0.0';
+const port = 41234;
+
+socket.bind(port, function() {
+	console.log('Joining multicast group ' + multicastAddress);
+	socket.addMembership(multicastAddress);
+});
+
+socket.on('message', function(msg, source) {
+	console.log('Data has arrived: ' + msg + '. Src IP: ' + source.address + '. Src port: ' + source.port);
+});
