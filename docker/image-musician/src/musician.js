de836d9307f3143ddc389c6f745acf01c926d091
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-06-05 14:34:32 +0200

    Musician : Update dockerfile (pass arg to script) and update script js

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,11 +1,91 @@
-const dgram = require('dgram');
-const socket = dgram.createSocket('udp4');
-const multicastAddress = '239.255.0.0';
-const port = 41234;
+var availableInstruments = [
+	{
+		name: "piano",
+		sound: "ti-ta-ti"
+	},
+	{
+		name: "trumpet",
+		sound: "pouet"
+	},
+	{
+		name: "flute",
+		sound: "trulu"
+	},
+	{
+		name: "violin",
+		sound: "gzi-gzi"
+	},
+	{
+		name: "drum",
+		sound: "boum-boum"
+	}
+];
 
-var message = Buffer.from('Je suis un musicien');
+// Fetch the command line arguments, remove 2 first (node and script name)
+var cmdArgs = process.argv.slice(2);
 
-socket.send(message, 0, message.length, port, multicastAddress, function(err, bytes) {
-	console.log('Sending');
-	socket.close();
-});
+// Must have ONE argument (the instrument name)
+if(cmdArgs.length != 1){
+	displayArgsError(availableInstruments);
+}else{
+	var instrument = null;
+
+	// Fetch the instrument passed in argument from the available instrument list object
+	for(var i = 0; i < availableInstruments.length; ++i){
+		if(availableInstruments[i].name == cmdArgs[0]){
+			instrument = availableInstruments[i];
+			break;
+		}
+	}
+
+	// Valid instrument
+	if(instrument != null){
+		createDgramSocket(instrument);
+	}
+	// Invalid instrument
+	else{
+		displayArgsError(availableInstruments);
+	}
+}
+
+
+
+
+/* 
+ * Display en invalid argument error, and show the available instruments
+ */
+function displayArgsError(availableInstruments){
+	var errorMsg = "Invalid argument => [";
+	for(var i = 0; i < availableInstruments.length; ++i){
+		if(i){
+			errorMsg += ", ";
+		}
+		errorMsg += availableInstruments[i].name;
+	}
+	errorMsg += "]";
+
+	console.log(errorMsg);
+}
+
+
+
+
+
+/* 
+ * Create a datagram socket and send UDP message
+ */
+function createDgramSocket(instrument) {
+	const dgram = require('dgram');
+	const socket = dgram.createSocket('udp4');
+	const multicastAddress = '239.255.0.0';
+	const port = 41234;
+
+	// Msg
+	var message = Buffer.from('Je suis un ' + instrument.name + " -> " + instrument.sound);
+
+	// Send
+	socket.send(message, 0, message.length, port, multicastAddress, function(err, bytes) {
+		console.log('Sending');
+		socket.close();
+	});
+}
